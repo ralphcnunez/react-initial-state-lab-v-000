a60977b501917927d043c3a1ae35e2196ba96d73
Ralph Cuello <ralphcuello1016@gmail.com>
2018-10-25 17:04:02 +0000

    automatically backed up by learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,13 +1,8 @@
 // your ImageSlider code here!
 
 import React, { Component } from 'react';
+import React, {Component} from 'react'
 
  export default class ImageSlider extends Component {
-  
-  render() {
-    return (
-      <h1>Hello</h1>
-      );
-  }
- }
+
  
